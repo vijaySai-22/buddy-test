022dbc539976be63a168355382b6a01d95c849cd
Pujari Vijay Sai Prasad <62492984+vijaySai-22@users.noreply.github.com>
2023-09-20 13:13:05 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World, buddy</h1>
+      <h1>Hello World, buddy!</h1>
     </div>
   );
 }
